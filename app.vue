5eeb757e9cecde5ba8507ba30736337a3c1751b6
James Coliz <jcoliz@outlook.com>
2024-11-01 15:41:47 -0700

    feat: Add simple page structure

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: "en",
+    "data-bs-theme": "dark"
+  },
+});
+</script>
