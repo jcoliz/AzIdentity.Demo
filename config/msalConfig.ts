0bd6c2a02c02f9eb885f3db0492df5a5605229f1
James Coliz <jcoliz@outlook.com>
2024-11-08 21:00:12 -0800

    build: Remove client-specific config out of code

--- config/msalConfig.ts
+++ config/msalConfig.ts
@@ -11,10 +11,11 @@ import {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
  */
 export const msalConfig = {
+    // Thse will be overridden by app config
     auth: {
-        clientId: 'fc9ca57e-4cc4-443b-99c4-495272142075', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://login.microsoftonline.com/organizations', // Replace the placeholder with your tenant subdomain        
-        redirectUri: 'http://localhost:3000/', // You must register this URI on Microsoft Entra admin center/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
+        clientId: '', // This is the ONLY mandatory field that you need to supply.
+        authority: '', // Replace the placeholder with your tenant subdomain        
+        redirectUri: '', // You must register this URI on Microsoft Entra admin center/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
     },
     cache: {
         cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO.
