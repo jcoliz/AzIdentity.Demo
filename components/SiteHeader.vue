763b3c7fd66101576fd22f87ae5e9f4dd101d601
James Coliz <jcoliz@outlook.com>
2024-11-15 16:07:00 -0800

    fix: Use routerlink for header site link
    Avoids needless refresh, which logs you out

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <RouterLink to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <FeatherIcon icon="users" size="32"/>
         <span class="ms-3 fs-4 me-3">AzIdentity.Demo</span>
-      </a>
+      </RouterLink>
 
       <ul class="nav nav-pills">
         <RouterLink class="nav-link" to="/">Home</RouterLink>
