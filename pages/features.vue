925b98080bf357df553879e9757d8227a12b2fd9
James Coliz <jcoliz@outlook.com>
2024-11-27 13:26:34 -0800

    feat: Simple change to features page as a test

--- pages/features.vue
+++ pages/features.vue
@@ -74,7 +74,7 @@ watch(accountCp, (val)=>{
 </script>
 
 <template>
-    <BasePageHeader>Features</BasePageHeader>
+    <BasePageHeader>Features!</BasePageHeader>
 
     <div v-if="!identityStore.account">
         <p>This page presents a number of additional things you can do once you are logged in.</p>
