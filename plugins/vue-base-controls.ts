853b346e95a5b011b2193fb9f1745eb8c7df93b9
James Coliz <jcoliz@outlook.com>
2024-11-27 16:51:05 -0800

    refactor: Call `vueApp.use` on controls plugin
    This will register all controls at once

--- plugins/vue-base-controls.ts
+++ plugins/vue-base-controls.ts
@@ -1,5 +1,5 @@
-import * as controls from '@coliz/vue-base-controls'
+import controls from '@coliz/vue-base-controls'
 
 export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.vueApp.component('BaseButton', controls.BaseButton)
+    nuxtApp.vueApp.use(controls)
 })
