1c87084ca8c38d1088cc77eb166494ac1e5e29b3
James Coliz <jcoliz@outlook.com>
2024-11-30 10:20:25 -0800

    feat: Add route and site title to browser tab title

--- components/PageMain.vue
+++ components/PageMain.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+const appConfig = useAppConfig()
+useHead({
+    title: `${route.meta.title} | ${appConfig.name}`
+})
+</script>
 <template>
     <main class="container">
         <slot/>
